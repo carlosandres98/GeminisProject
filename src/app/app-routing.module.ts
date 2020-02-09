93e159b88c25053b276fec324a02769340ecdb0e
Mateo Orozco Lotero <35343266+molinsocorp@users.noreply.github.com>
2020-02-09 11:18:23 -0500

    01-MOL
    Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
 
   {path: 'login', component: LoginComponent},
+  {path : 'register', component : RegisterComponent },
   {path: 'dashboard', component: DashboardComponent},
   {path: '*', component: LoginComponent}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+  
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
