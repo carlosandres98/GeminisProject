be1aca9a98a84cf0ccc17ae30ab695cea4e125e0
Mateo Orozco Lotero <35343266+molinsocorp@users.noreply.github.com>
2020-02-10 10:14:27 -0500

    UpLoad 16 - MOL

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -9,11 +9,15 @@ import { ProductServiceService, ProductI } from '../../services/product-service.
 })
 export class CarComponent implements OnInit {
 
+  public totalPay : number;
   public countProducts : number;
   public CarList : ProductI [] = null;
   constructor(private act:ActivatedRoute, private productService:ProductServiceService) { 
     this.CarList = productService.getoffertWeekProductList();
     this.countProducts = this.CarList.length;
+    for (let product of this.CarList){
+      this.totalPay = this.totalPay+product.price;
+    }
   }
 
   ngOnInit() {
