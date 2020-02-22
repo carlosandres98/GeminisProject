1ba4388d34a1bc3e237cbc78c95ca8314a036700
Mateo Orozco Lotero <35343266+molinsocorp@users.noreply.github.com>
2020-02-22 14:05:43 -0500

    Update functionalities.service.ts

--- src/app/services/functionalities.service.ts
+++ src/app/services/functionalities.service.ts
@@ -7,14 +7,29 @@ import { ProductI, ProductServiceService } from './product-service.service';
 export class FunctionalitiesService {
 
   public likeCategories: string[];
-  public producList: ProductI[];
+  public productList: ProductI[] = [];
+  public productListLike: ProductI[];
 
   constructor(private producService: ProductServiceService) {
-    this.producList = producService.getAllProducts();
+    this.productList = producService.getAllProducts();
 
   }
-  go(a: string[]) {
-    this.likeCategories = a;
-    console.log(this.likeCategories);
+  go(like: string[]) {
+    this.likeCategories = like;
+    
+    console.log(this.likeProducts());
+  }
+
+
+  public likeProducts = () => {
+    this.productList = [];
+    for (let i of this.likeCategories) {
+      for (let product of this.productList){
+        if (product.category==i){
+          this.productListLike.push(product);
+        }
+      }
+    }
+    return this.productListLike;
   }
 }
