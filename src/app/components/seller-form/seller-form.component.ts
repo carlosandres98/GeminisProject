ca9752c8fbb9526f1af5a3be788b1435437ef667
Mateo Orozco Lotero <35343266+molinsocorp@users.noreply.github.com>
2020-02-26 11:31:08 -0500

    UpLoad 69 - MOL

--- src/app/components/seller-form/seller-form.component.ts
+++ src/app/components/seller-form/seller-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { usersDBI } from '../../services/user-service.service';
+import { usersDBI, UserServiceService } from '../../services/user-service.service';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./seller-form.component.scss']
 })
 export class SellerFormComponent implements OnInit {
+
+  user: usersDBI;
 
   userDB: usersDBI = {
     city: '',
@@ -25,13 +27,33 @@ export class SellerFormComponent implements OnInit {
     confirmpass: ''
 
   };
-  constructor() { }
+  constructor(private userSerice: UserServiceService) {
+
+  }
 
   ngOnInit() {
   }
 
   goRegister(formData: NgForm) {
-    console.log(formData);
+    this.userDB = this.userSerice.getUserByEmail(this.userDB.email);
+    this.user = {
+      city: formData.value.txtCity,
+      country: formData.value.txtCountry,
+      address: formData.value.txtAddress,
+      services: formData.value.txtServices,
+      phone : formData.value.txtPhone,
+      id: formData.value.txtId,
+      photo: this.userDB.photo,
+      accountype: this.userDB.accountype,
+      names: this.userDB.names,
+      mode: this.userDB.mode,
+      lastnames: this.userDB.lastnames,
+      email: this.userDB.email,
+      password: this.userDB.password,
+      confirmpass: this.userDB.confirmpass
+    }
+    console.log(this.user);
+
   }
 
 }
