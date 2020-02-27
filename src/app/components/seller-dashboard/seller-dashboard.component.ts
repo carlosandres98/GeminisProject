01d3f6804ddded4126bc059494ae797f8d798cf7
Mateo Orozco Lotero <35343266+molinsocorp@users.noreply.github.com>
2020-02-27 12:10:25 -0500

    UpLoad 76 - MOL

--- src/app/components/seller-dashboard/seller-dashboard.component.ts
+++ src/app/components/seller-dashboard/seller-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductI } from '../../services/product-service.service';
 
 @Component({
   selector: 'app-seller-dashboard',
@@ -8,6 +9,52 @@ import { Component, OnInit } from '@angular/core';
 export class SellerDashboardComponent implements OnInit {
 
   constructor() { }
+
+  options : any [] = [
+    {
+      img : 'assets/img/products/electrodomesticos01.png',
+      text : 'Vende Productos',
+      sales : '1 K',
+      followers : '520',
+      c1 : {
+        txt1 : 'Ventas: ',
+        txt2 : 'Seguidores: '
+      }
+    },
+    {
+      img : 'assets/img/products/electrodomesticos01.png',
+      text : 'Tus Productos',
+      sales : '2 k',
+      followers : '720',
+      c1 : {
+        txt1 : 'Productos: ',
+        txt2 : 'Gustados: '
+      }
+    },
+    {
+      img : 'assets/img/products/electrodomesticos01.png',
+      text : 'Editar Productos',
+      sales : '2 k',
+      followers : '720',
+      c1 : {
+        txt1 : 'Eliminados: ',
+        txt2 : 'No vendidos: '
+      }
+    },
+    {
+      img : 'assets/img/products/electrodomesticos01.png',
+      text : 'Tus Compras',
+      sales : '2 k',
+      followers : '720',
+      c1 : {
+        txt1 : 'Compras: ',
+        txt2 : 'Ofertas: '
+      }
+    }
+
+  ];
+
+  
 
   ngOnInit() {
   }
